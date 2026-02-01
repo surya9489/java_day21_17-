409983d7d231d659b1f3fdb5ce1d2ec2a18ecbc4
Surya Prakash <suryaprakashsp9489@gmail.com>
2026-02-01 15:42:08 +0530

    Update Callablestmt.sql

--- Callablestmt.sql
+++ Callablestmt.sql
@@ -10,3 +10,39 @@ CREATE TABLE Student (
 SHOW TABLES;
 DESC Student;
 SELECT * FROM Student;
+
+TRUNCATE TABLE Student;
+
+-----------------------------------------------------------------
+
+USE callable;
+
+DELIMITER //
+
+CREATE PROCEDURE addStudent(
+    IN sid INT,
+    IN sname VARCHAR(100),
+    IN smarks INT
+)
+BEGIN
+    INSERT INTO Student VALUES (sid, sname, smarks);
+END //
+
+DELIMITER ;
+
+---------------------------------------------------------------------
+
+USE callable;
+
+DELIMITER //
+
+CREATE PROCEDURE getStudentById(IN sid INT)
+BEGIN
+    SELECT * FROM Student
+    WHERE id = sid;
+END //
+
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS getStudentById;
+
